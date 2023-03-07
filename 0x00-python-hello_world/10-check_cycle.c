4ecfc617627d4fd2627c7200b7e0ddf3cd590317
seedumar <seedumar1999@gmail.com>
2023-03-06 16:29:45 -0800

    Python

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,15 +3,15 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+* print_listint - prints all elements of a listint_t list
+* @h: pointer to head of list
+* Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *current;
-	unsigned int n;
-	/* number of nodes */
+	unsigned int n; /* number of nodes */
+
 	current = h;
 	n = 0;
 	while (current != NULL)
@@ -20,6 +20,7 @@ size_t print_listint(const listint_t *h)
 		current = current->next;
 		n++;
 	}
+
 	return (n);
 }
 
@@ -36,9 +37,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
 	new->next = *head;
 	*head = new;
+
 	return (new);
 }
 
