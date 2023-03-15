e664c86c243901052ca09896087ad4060e81e0eb
seedumar <seedumar1999@gmail.com>
2023-03-15 12:58:04 +0000

    Python

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-
 def replace_in_list(my_list, idx, element):
-    if my_list is not None and -1 < idx < len(my_list):
+    if idx < 0 or idx >= len(my_list):
+        return (my_list)
+    else:
         my_list[idx] = element
-    return 
+        return (my_list)
