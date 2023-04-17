3f28d1a80c1ccc188c63189c447136b9da62d61c
seedumar <seedumar1999@gmail.com>
2023-04-17 11:51:19 +0000

    Python Exceptions

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        jm = a / b
+    except (TypeError, ZeroDivisionError):
+        jm = None
+    finally:
+        print("Inside result: {}".format(jm))
+        return (jm)
